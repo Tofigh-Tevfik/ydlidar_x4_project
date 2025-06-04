66594f0a1e34e580d525e8c17d9e3fa7066e786e
Tevfik Tofigh <tofigh.github@gmail.com>
2025-06-04 23:26:37 +0300

    Add types.hpp for ZMQ messaging

---
+++ include/types.hpp
@@ -0,0 +1,18 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <vector>
+#include <cstdint>
+
+// a struct for a single data point of lidar
+struct LidarPoint {
+    float angle; // angle in degrees
+    float distance; // distance in millimeters
+};
+
+struct LaserScan {
+    std::vector<LidarPoint> points; // a vector of lidar points
+    uint64_t timestamp; // timestamp in milliseconds
+}
+
+#endif
