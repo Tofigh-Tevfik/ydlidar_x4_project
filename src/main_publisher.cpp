68a6f7d8c61bb45816812e4996c728d2231fb490
Tevfik Tofigh <tofigh.github@gmail.com>
2025-06-08 23:15:12 +0300

    Add main publisher/subscriber source files

---
+++ src/main_publisher.cpp
@@ -0,0 +1,23 @@
+#include "ydlidar_x4_reader.hpp"
+#include "publisher.hpp"
+#include "iostream"
+
+int main() {
+    try {
+        YDLidarX4Reader lidar;
+        Publisher pub;
+        std::cout << "Publishing Lidara data on tcp://*:5555..." << std::endl;
+        while(true) {
+            LidarScan scan = lidar.parseData();
+            pub.publish(scan);
+            for (const auto& point : scan.points) {
+                printf("Published - Angle: %.2f deg, Distance: %.2f mm\n", point.agnle, point.distance);
+            }
+            usleep(1000); // 1ms delay
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
---
+++ src/main_subscriber.cpp
@@ -0,0 +1,23 @@
+#include "subscriber.hpp"
+#include <iostream>
+
+int main() {
+    try {
+        Subscriber sub;
+        std::cout << "Subscribing to Lidar data on tcp:://localhost:5555..." << std::endl;
+        while(true) {
+            LidarScan scan;
+            if (sub.callback(scan)) {
+                std::cout << "Received scan at timestamp: " << scan.timestamp << " us" <, std::endl;
+                for (const auto& point : scan.points) {
+                    printf("Angle: %.2f deg, Distance: %.2f mm\n", point.angle, point.distance);
+                }
+                usleep(1000); // 1ms delay
+            }
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
